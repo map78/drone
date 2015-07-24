beb8ab5ed726c913751a8534c6e4d3a43bc8fc60
V3498 (Conference ID) <v3498@cam.ac.uk>
2015-07-24 14:35:57 +0100

    Sixth Attempt

--- drone-backend.js
+++ drone-backend.js
@@ -25,7 +25,7 @@ function fly(robot) {
     bot.drone.ftrim();
     bot.drone.takeoff();
     after(5*1000, function () {
-        bot.drone.left(0.2)
+        bot.drone.left(0.05)
     });
     after(5.8*1000, function() {
         bot.drone.left(0)
@@ -33,31 +33,31 @@ function fly(robot) {
     after(5.8*1000, function () {
         bot.drone.front(0.1)
     });
-    after(9*1000, function () {
+    after(12*1000, function () {
         bot.drone.front(0)
     });
-    after(9*1000, function() {
+    after(12*1000, function() {
         bot.drone.right(0.2)
     });
-    after(10*1000, function() {
+    after(14*1000, function() {
         bot.drone.right(0)
     });
-    after(10*1000, function() {
+    after(14*1000, function() {
         bot.drone.back(0.1)
     });
-    after(14*1000, function() {
+    after(22*1000, function() {
         bot.drone.back(0)
     });
-    after(14*1000, function() {
+    after(22*1000, function() {
         bot.drone.left(0.1)
     });
-    after(15*1000, function() {
+    after(23*1000, function() {
         bot.drone.left(0)
     });
-    after(16*1000, function() {
+    after(24*1000, function() {
         bot.drone.land();
     });
-    after(20*1000, function() {
+    after(25*1000, function() {
         bot.drone.stop();
     });
 }
